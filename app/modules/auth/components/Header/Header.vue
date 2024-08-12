29408e5841bb13304d97e20986830fa6a7950de1
Diener Dornelas <diener.ld@outlook.com>
2024-08-12 13:18:47 -0300

    refactor(login): change local bg color

--- app/modules/auth/components/Header/Header.vue
+++ app/modules/auth/components/Header/Header.vue
@@ -2,8 +2,8 @@
 </script>
 
 <template>
-  <header class="flex w-full justify-center shadow">
-    <nav class="bg-white px-4 py-2.5 lg:px-6">
+  <header class="flex w-full justify-center bg-white shadow">
+    <nav class="px-4 py-2.5 lg:px-6">
       <div class="mx-auto flex max-w-screen-xl items-center justify-center md:justify-between">
         <NuxtLink to="/" class="flex items-center">
           <Logo />
