f0db6ad1bb30857de6b541e013008a48e92af824
Diener Dornelas <diener.ld@outlook.com>
2024-08-12 13:18:10 -0300

    feat(app:home): create header with menu options

--- app/modules/app/components/Header/Header.vue
+++ app/modules/app/components/Header/Header.vue
@@ -1,7 +1,46 @@
 <script setup lang="ts">
+const props = withDefaults(defineProps<{
+  name: string
+}>(), {
+  name: 'Apartamento'
+})
+
 const emit = defineEmits<{
-  (e: 'wants-to-login'): void
+  (e: 'wants-to-signout'): void
+  (e: 'wants-to-profile'): void
 }>()
+
+const firstLetter = computed(() => {
+  let result = ''
+  const [first, last] = props.name.trim().split(' ')
+  if (first) { result = first.charAt(0).toUpperCase() }
+
+  if (last) { result += last.charAt(0).toUpperCase() }
+
+  return result
+})
+
+const menu = ref()
+const items = ref([
+  {
+    items: [
+      {
+        label: 'Perfil',
+        icon: 'pi pi-user',
+        command: () => emit('wants-to-profile')
+      },
+      {
+        label: 'Sair',
+        icon: 'pi pi-sign-out',
+        command: () => emit('wants-to-signout')
+      }
+    ]
+  }
+])
+
+const toggle = (event: MouseEvent) => {
+  menu.value.toggle(event)
+}
 </script>
 
 <template>
@@ -13,11 +52,16 @@ const emit = defineEmits<{
         </NuxtLink>
         <div class="hidden items-center md:flex">
           <Button
-            label="Entrar"
-            icon-pos="right"
-            icon="pi pi-sign-in"
-            @click="() => emit('wants-to-login')"
-          />
+            type="button"
+            aria-label="Menu"
+            aria-haspopup="true"
+            rounded
+            aria-controls="overlay_menu"
+            @click="toggle"
+          >
+            {{ firstLetter }}
+          </Button>
+          <Menu id="overlay_menu" ref="menu" :model="items" :popup="true" />
         </div>
       </div>
     </nav>
