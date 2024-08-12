009d9eaa32cc873b111925c472b21d590d6d2a8d
Diener Dornelas <diener.ld@outlook.com>
2024-08-12 13:17:10 -0300

    refactor: adjust default styles

--- app/components/MainContent/MainContent.vue
+++ app/components/MainContent/MainContent.vue
@@ -1,10 +1,23 @@
+<script setup lang="ts">
+const props = defineProps<{
+  innerScroll?: boolean
+}>()
+</script>
+
 <template>
   <div class="flex size-full  min-w-96 flex-col">
     <slot name="header" class="flex-1" />
-    <div class="size-full flex-1 bg-surface-50">
-      <div class="mx-auto size-full max-w-screen-xl overflow-auto p-1 sm:p-4 lg:p-6">
+    <div class="mb-2 size-full flex-1 bg-surface-50" :class="{ 'overflow-auto': props.innerScroll }">
+      <div class="mx-auto size-full max-w-screen-xl p-1 sm:p-4 lg:p-6">
         <slot name="content" />
       </div>
     </div>
+    <!-- footer -->
+    <!-- footer default -->
+    <footer class=" flex w-full bg-slate-200">
+      <slot name="footer">
+        <p>&copy; {{ new Date().getFullYear() }}</p>
+      </slot>
+    </footer>
   </div>
 </template>
