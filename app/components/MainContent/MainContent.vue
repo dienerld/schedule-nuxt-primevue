0fc7c6af95ce5a355bead278f1e406169eb06dc7
Diener Dornelas <diener.ld@outlook.com>
2024-08-12 05:05:10 -0300

    feat: adjust CSS to improve responsiveness

--- app/components/MainContent/MainContent.vue
+++ app/components/MainContent/MainContent.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="flex size-full flex-col">
+  <div class="flex size-full  min-w-96 flex-col">
     <slot name="header" class="flex-1" />
     <div class="size-full flex-1 bg-surface-50">
-      <div class="mx-auto size-full max-w-screen-xl p-5">
+      <div class="mx-auto size-full max-w-screen-xl overflow-auto p-1 sm:p-4 lg:p-6">
         <slot name="content" />
       </div>
     </div>
