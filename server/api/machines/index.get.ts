864943009190afe77431866e3bf53dbf6a476991
Diener Dornelas <diener.ld@outlook.com>
2024-08-19 20:58:19 -0300

    feat(api:machine): get available machine for date and shift

--- server/api/machines/index.get.ts
+++ server/api/machines/index.get.ts
@@ -1,11 +1,16 @@
 import * as Luxon from 'luxon';
-
+import { z } from 'zod';
+import { zh } from 'h3-zod';
 export default eventHandler(async (event) => {
   const db = useDB();
-  const { date: dateTimestamp } = getQuery(event);
-  const luxonDate = Luxon.DateTime.fromJSDate(
-    new Date(Number(dateTimestamp?.toString())),
-  ).set({
+  const { date: dateTimestamp, shift } = await zh.useValidatedQuery(
+    event,
+    z.object({
+      date: z.coerce.number(),
+      shift: z.enum(['afternoon', 'morning']),
+    }),
+  );
+  const luxonDate = Luxon.DateTime.fromJSDate(new Date(dateTimestamp)).set({
     hour: 0,
     minute: 0,
     second: 0,
@@ -17,14 +22,32 @@ export default eventHandler(async (event) => {
     end: luxonDate.plus({ days: 1 }).toJSDate().getTime(),
   };
 
-  // busca maquinas que estão vazias no dia $1
+  /**
+   * Busca máquinas que estão habilitadas e não possuem agendamentos no intervalo de data especificado (date.start e date.end) para o turno especificado (shift).
+   * A consulta utiliza um LEFT JOIN para associar máquinas com seus agendamentos, e o filtro `isNull(tables.schedule.id)` garante que apenas as máquinas
+   * que sem agendamentos para o período e turno especificados sejam retornadas. Isso permite identificar as máquinas disponíveis para novos agendamentos.
+   */
   const machines = await db
-    .select()
+    .select({
+      id: tables.machine.id,
+      name: tables.machine.name,
+      enabled: tables.machine.enabled,
+    })
     .from(tables.machine)
-    .rightJoin(tables.schedule, eq(tables.machine.id, tables.schedule.machine))
-    .where(between(tables.schedule.day, date.start, date.end));
-
-  console.log(machines);
+    .leftJoin(
+      tables.schedule,
+      and(
+        eq(tables.machine.id, tables.schedule.machine),
+        eq(tables.schedule.shift, shift),
+        between(tables.schedule.day, date.start, date.end),
+      ),
+    )
+    .where(
+      and(
+        eq(tables.machine.enabled, true),
+        isNull(tables.schedule.id), // Filtra para máquinas que não têm agendamento
+      ),
+    );
 
   return machines;
 });
