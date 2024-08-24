6d39a6bd0333bd34a4e1a0dab678fb019129ec1b
Diener Dornelas <diener.ld@outlook.com>
2024-08-23 21:47:26 -0300

    feat: add validation if user already booking in cooldown range

--- server/api/schedules/index.post.ts
+++ server/api/schedules/index.post.ts
@@ -1,12 +1,10 @@
-import * as luxon from 'luxon';
 import { between } from 'drizzle-orm';
-import { zh } from 'h3-zod';
-import { z } from 'zod';
+import { zh, z } from 'h3-zod';
 import { authMiddleware } from '~~/server/middlewares/auth.middleware';
 
 export default eventHandler(async (event) => {
   await authMiddleware(event);
-
+  const { schedules: scheduleConfig } = useRuntimeConfig().app;
   const db = useDB();
   const { user } = await getUserSession(event);
   const body = await zh.useValidatedBody(
@@ -18,36 +16,22 @@ export default eventHandler(async (event) => {
     }),
   );
 
+  const day = useLuxon(body.day);
+
   if (!user) {
     throw createError({
       statusCode: 401,
       message: 'Unauthorized',
     });
   }
-  const luxonDate = luxon.DateTime.fromJSDate(new Date(body.day)).set({
-    hour: 0,
-    minute: 0,
-    second: 0,
-    millisecond: 0,
-  });
-  const day = {
-    start: luxonDate.toJSDate().getTime(),
-    end: luxonDate
-      .set({
-        hour: 23,
-        minute: 59,
-        second: 59,
-      })
-      .toJSDate()
-      .getTime(),
-  };
+
   const schedule = await db
     .select()
     .from(tables.schedule)
     .where(
       and(
         eq(tables.schedule.machine, body.machine),
-        between(tables.schedule.day, day.start, day.end),
+        between(tables.schedule.day, day.start.toMillis(), day.end.toMillis()),
         eq(tables.schedule.shift, body.shift),
       ),
     )
@@ -56,7 +40,39 @@ export default eventHandler(async (event) => {
   if (schedule) {
     throw createError({
       statusCode: 409,
-      message: 'Schedule already exists',
+      message: 'Agendamento já existe.',
+    });
+  }
+
+  const dateBetween = useLuxon(body.day);
+
+  dateBetween.start = dateBetween.start.minus({
+    days: scheduleConfig.cooldownInDays,
+  });
+  dateBetween.end = dateBetween.end.plus({
+    days: scheduleConfig.cooldownInDays,
+  });
+
+  const nearestSchedules = await db
+    .select({
+      day: tables.schedule.day,
+    })
+    .from(tables.schedule)
+    .where(
+      and(
+        eq(tables.schedule.userId, user.id),
+        between(
+          tables.schedule.day,
+          dateBetween.start.toMillis(),
+          dateBetween.end.toMillis(),
+        ),
+      ),
+    );
+
+  if (nearestSchedules.length > 0) {
+    throw createError({
+      statusCode: 400,
+      message: 'Você possui agendamentos próximos, não é possível agendar.',
     });
   }
 
